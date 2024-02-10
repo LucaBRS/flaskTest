4a92dc99e2133549258ef7ff2d0f8085af5533fa
lucab <luca.barsottini@outlook.it>
2024-02-10 20:53:15 +0100

    comment

--- app.py
+++ app.py
@@ -3,23 +3,24 @@
 # todo vedere come aggiungiere openAPI
 
 import logging
-import toml
-from flask import Flask, render_template, url_for, request, redirect
 
-#l+ SQLAlchemy provides a set of tools and utilities for working with databases from flask_sqlalchemy import SQLAlchemy from datetime import datetime
-from src.main.model.task_model import db, TaskModule  # Import db from the model module
+import toml
+from flask import Flask
+
 from src.main.controller.task_controller import TaskController
+# l+ SQLAlchemy provides a set of tools and utilities for working with databases from flask_sqlalchemy import  SQLAlchemy from datetime import datetime
+from src.main.model.task_model import db  # Import db from the model module
 
 config = toml.load('config.toml')
 
-#l+ creating a Flask application instance named app __name__ parameter is a special variable that represents the name of the current module
+# l+ creating a Flask application instance named app __name__ parameter is a special variable that represents the name of the current module
 app = Flask(__name__)
 app.template_folder = config['flask']["template_folder"]
 app.static_folder = config['flask']["static_folder"]
 app.instance_path=config['flask']["instance_path"]
-#l+ configuring Flask application to use a SQLite database named task.db app.config: This is a dictionary-like object provided by Flask to store configuration settings for your application ['SQLALCHEMY_DATABASE_URI']: This accesses a specific configuration option within the app.config dictionary 'sqlite:///task.db': This is the database URI that specifies the connection details for the SQLite database
+# l+ configuring Flask application to use a SQLite database named task.db app.config: This is a dictionary-like object provided by Flask to store configuration settings for your application ['SQLALCHEMY_DATABASE_URI']: This accesses a specific configuration option within the app.config dictionary 'sqlite:///task.db': This is the database URI that specifies the connection details for the SQLite database
 app.config['SQLALCHEMY_DATABASE_URI'] = config['flask']["database_uri"]
-#l+  explicitly associate the db instance with your Flask application by calling db.init_app(app). This allows Flask-SQLAlchemy to work within the context of your Flask application.
+# l+  explicitly associate the db instance with your Flask application by calling db.init_app(app). This allows Flask-SQLAlchemy to work within the context of your Flask application.
 db.init_app(app)
 logging.basicConfig(level=logging.DEBUG)
 logger = app.logger
