17cb2e09e9d40424c910986f44e529ea4b7227e7
lucab <luca.barsottini@outlook.it>
2024-02-16 16:15:26 +0100

    add sys.arg to choose config file for database

--- src/main/config/sessions_configuration.py
+++ src/main/config/sessions_configuration.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 import toml
 from main.model.gas_station_model import GasStation
@@ -6,7 +7,21 @@ from main.model.person_model import Person
 from main.model.task_model import Task
 from config.database_creation import DatabaseCreation
 
-#TODO evitare classe e "scrivere diretto"
+
+def parse_arguments(args):
+    config_file = None
+    for arg in args:
+        if arg.startswith('CONFIG_FILE='):
+            config_file = arg.split('=')[1]
+    return config_file
+
+config_file = parse_arguments(sys.argv[1:])
+
+if config_file:
+    print("CONFIG_FILE argument provided:", config_file)
+else:
+    print("CONFIG_FILE argument not provided. Setting to default config.")
+    config_file = 'config.toml'
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 config_file_path = os.path.join(current_dir,'..', '..', '..', 'config.toml')
